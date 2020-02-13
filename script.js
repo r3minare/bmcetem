d35a84f276443edf221d267476bdf89e7a00f625
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2020-02-13 14:00:22 -0300

    :)

--- script.js
+++ script.js
@@ -19,10 +19,13 @@ var EQUIPAMENTOS = [
 var Form = document.getElementById('formulario');
 
 /* ----------EQUIPAMENTOS-------- */
-for(let i=1;i<EQUIPAMENTOS.length;i++){
+var Equip = document.getElementById('equip');
+for(let i=0;i<EQUIPAMENTOS.length;i++){
     let S = EQUIPAMENTOS[i];
-    Form.innerHTML += (
+    Equip.innerHTML += (
         '<input type="radio" id="'+S+'" name="equipamento" value="'+S+'"> </input>'+
         '<label for="'+S+'">'+S+'</label><br>'
     );
 }
+
+/* -------MATERIAIS-------- */
